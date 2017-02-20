bcd26aa84630c89f30a3a0ee8aa6523f8ecf5c54
Koen Torfs <ktorfs@gmail.com>
2017-02-20 10:27:15 +0100

    fixes typo and annotation field removal

--- app/models/concerns/foreman_vmwareannotations/vmware_extensions.rb
+++ app/models/concerns/foreman_vmwareannotations/vmware_extensions.rb
@@ -3,7 +3,7 @@ module ForemanVmwareannotations
     extend ActiveSupport::Concern
 
     def host_compute_attrs(host)
-      super(host).merge(:annotations => host.comment)
+      super(host).merge(:annotation => host.comment)
     end
   end
 end
--- app/overrides/remove_annotations_field.rb
+++ app/overrides/remove_annotations_field.rb
@@ -1,5 +1,5 @@
-Deface::Override.new(
-  :name => 'remove_annotations_field',
-  :virtual_path => 'app/views/compute_resources_vms/form/vmware/_base',
-  :remove => "#host_compute_attributes_annotation"
+Deface::Override.new(:virtual_path => "compute_resources_vms/form/vmware/_base",
+  :name => "remove_annotations_field",
+  :remove => "erb[loud]:contains('annotation')",
+  :original => 'cadd97e0a38a36d4464160a972191df90aa6610f'
 )
